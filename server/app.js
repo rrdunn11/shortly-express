68de9a79d61eb8da4b840357e7cfb6f1abd4379f
rrdunn11 <rrdunn@gmail.com>
2019-05-03 18:57:40 -0700

    Fix indentation

--- server/app.js
+++ server/app.js
@@ -17,117 +17,117 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {//check if link already exists; if exists, then throw link
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {//check if link already exists; if exists, then throw link
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
-app.post('/signup', 
-(req, res, next) => {
-  // var url = req.body.url;
-  // console.log(req.body.username, req.body.password);
+app.post('/signup',
+  (req, res, next) => {
+    // var url = req.body.url;
+    // console.log(req.body.username, req.body.password);
 
-  if (!req.body.username || !req.body.password) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+    if (!req.body.username || !req.body.password) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
 
-  var options = {
-    username: req.body.username,
-    password: req.body.password
-  };
+    var options = {
+      username: req.body.username,
+      password: req.body.password
+    };
 
-  return models.Users.get(options)
-    .then(user => {//check if link already exists; if exists, then throw link
-     console.log(user)
-      if (user) {
-        throw user;
-      }
-      return user
-      // models.Users.getUrlTitle(url);
-    })
-    .then(user => {
-      console.log('hello')
-      return models.Users.create(options);
+    return models.Users.get(options)
+      .then(user => {//check if link already exists; if exists, then throw link
+        console.log(user);
+        if (user) {
+          throw user;
+        }
+        return user;
+        // models.Users.getUrlTitle(url);
+      })
+      .then(user => {
+        console.log('hello');
+        return models.Users.create(options);
 
-    })
-    .then(result => {
-      // res.redirect('/');
-      res.end();
-    })
-    .catch(user => {
-      res.redirect('/signup');
-      // res.status(200).end(user);
-    });
-  //   .then(results => {
-  //     return models.Links.get({ id: results.insertId });
-  //   })
-  //   .then(link => {
-  //     throw link;
-  //   })
-  //   .error(error => {
-  //     res.status(500).send(error);
-  //   })
-  //   .catch(link => {
-  //     res.status(200).send(link);
-  //   });
-});
+      })
+      .then(result => {
+        // res.redirect('/');
+        res.end();
+      })
+      .catch(user => {
+        res.redirect('/signup');
+        // res.status(200).end(user);
+      });
+    //   .then(results => {
+    //     return models.Links.get({ id: results.insertId });
+    //   })
+    //   .then(link => {
+    //     throw link;
+    //   })
+    //   .error(error => {
+    //     res.status(500).send(error);
+    //   })
+    //   .catch(link => {
+    //     res.status(200).send(link);
+    //   });
+  });
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
