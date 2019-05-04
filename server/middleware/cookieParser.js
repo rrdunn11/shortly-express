6db495e1d49d2a9d259dc19bdb71fa0c73d22cdc
rrdunn11 <rrdunn@gmail.com>
2019-05-03 20:05:50 -0700

    Implement parsing cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,15 @@
 const parseCookies = (req, res, next) => {
+  var cooky = req.headers.cookie;
+  req.cookies = {};
+
+  if (cooky !== undefined) {
+    cooky = cooky.split('; ');
+    for (var i = 0; i < cooky.length; i++) {
+      var baking = cooky[i].split('=');
+      req.cookies[baking[0]] = baking[1];
+    }
+  }
+  next();
 };
 
 module.exports = parseCookies;
